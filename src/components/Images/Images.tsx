7f074bcf449ef532f5baa8a0321c8a63d7d80c71
jathom30 <jathom30@gmail.com>
2022-03-15 21:43:30 -0400

    look for thumbnails from s3

--- src/components/Images/Images.tsx
+++ src/components/Images/Images.tsx
@@ -3,7 +3,7 @@ import ImageGallery from 'react-image-gallery';
 import './Images.scss'
 
 const s3Images = ['band', 'bigHandsV2', 'brick', 'danHead', 'garage', 'palace', 'palace1', 'palace2', 'sit', 'smallestToLargest', 'stand', 'walk']
-const items = s3Images.map(image => ({ original: `https://stn-pics.s3.us-east-2.amazonaws.com/${image}.jpg`, thumbnail: `https://stn-pics.s3.us-east-2.amazonaws.com/${image}.jpg`}))
+const items = s3Images.map(image => ({ original: `https://stn-pics.s3.us-east-2.amazonaws.com/${image}.jpg`, thumbnail: `https://stn-pics.s3.us-east-2.amazonaws.com/${image}-thumbnail.jpg`}))
 
 export const Images = () => {
   return (
